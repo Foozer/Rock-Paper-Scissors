ce59469bd03cd41ae00c7e2574c8b53f11e63e9e
Iain Watt <iainskwatt@gmail.com>
2021-06-28 13:12:06 +0100

    added pseudocode to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+//computer selects rock, paper or scissors and returns
+//that value.
+
+//computerPlay()
+//create random number to and assign to randomChoice-
+// This will be to select rock(1-10) paper(20-30) or scissors(30-40)
+//if randomChoice <= 10 then computerChoice = 'Rock'
+//else if randomChoice <= 30 then computerChoice = 'Paper'
+//else computerChoice = 'Scissors'
+//return computerChoice
+
+
+//playerSelection()
+//console asks for users choice and assings to playerChoice
+//return playerChoice
+
+//playRound()
+//evaluate users choice vs computers choice and return-
+//make both computerChoice and playerChoice the same case
+//If computerChoice = playerChoice then winner is a tie
+//Else if computerChoice = 'rock' and playerChoice = 'scissor':
+//  winner = computer
+//Else if computerChoice = 'paper' and playerChoice = 'rock':
+// winner = computer
+//Else if computerChoice = 'scissors' and playerChoice =  'paper':
+//  winner = computer
+//Else if playerChoice = 'rock' and computerChoice = 'scissor':
+//  winner = player
+//Else if playerChoice = 'paper' and computerChoice = 'rock':
+// winner = player
+//Else if playerChoice = 'scissors' and computerChoice =  'paper':
+//  winner = player
+
+//who won.
+//print winner
